8573edc45910ad0069e4c53f5ceb9229c0f4827f
rama-adi <17980909+rama-adi@users.noreply.github.com>
2022-10-08 18:24:55 +0700

    #1 wip

--- app/Models/QuizSession.php
+++ app/Models/QuizSession.php
@@ -4,8 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class QuizSession extends Model
 {
     use HasFactory;
+
+    public function quiz(): BelongsTo
+    {
+        return $this->belongsTo(Quiz::class);
+    }
+
+    public function sessionAnswers(): HasMany
+    {
+        return $this->hasMany(SessionAnswer::class);
+    }
 }
